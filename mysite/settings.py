b25ccf8fcb75f4cf992c1b331fa30a3f3faf53a7
Yasushi400 <chen20030223@outlook.jp>
2022-11-08 18:39:18 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o@96+lxy2^+b7a788x=l3elb4^wud4_((!&b8z&3rqc=fxn*=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'yasuhirochin.pythonanywhere.com']
 
